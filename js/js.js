8a6a2c12f6313a41af9084b3b9d3d28b4bf20eb9
GentYlli <96697816+GentYlli@users.noreply.github.com>
2023-09-23 18:02:38 +0200

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,9 @@
+//sidebar
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+  }
+  
