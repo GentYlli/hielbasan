2c2d8c23f2ef6abca5610eb80614c145b1b04e7d
GentYlli <96697816+GentYlli@users.noreply.github.com>
2023-10-09 15:47:38 +0200

    Delete js directory

--- js/js.js
+++
@@ -1,9 +0,0 @@
-//sidebar
-function openNav() {
-    document.getElementById("mySidebar").style.width = "250px";
-  }
-  
-  function closeNav() {
-    document.getElementById("mySidebar").style.width = "0";
-  }
-  
